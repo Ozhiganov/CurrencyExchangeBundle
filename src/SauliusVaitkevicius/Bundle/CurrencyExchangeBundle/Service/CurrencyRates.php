c1bd1fcd47099ec7c54d099e30c8fe7cd6e4bbd1
Saulius V <Saulius V>
2016-07-23 13:11:38 +0300

    refarctored services

--- src/SauliusVaitkevicius/Bundle/CurrencyExchangeBundle/Service/CurrencyRates.php
+++ src/SauliusVaitkevicius/Bundle/CurrencyExchangeBundle/Service/CurrencyRates.php
@@ -2,26 +2,62 @@
 
 namespace SauliusVaitkevicius\Bundle\CurrencyExchangeBundle\Service;
 
+use SauliusVaitkevicius\Bundle\CurrencyExchangeBundle\Entity\CurrencyExchangeRate;
 use Doctrine\ORM\EntityManager;
 
-class CurrencyRates
+class CurrencyRates 
 {
-    protected $repo;
-    
-    public function __construct(EntityManager $entityManager)
+    private $em;
+    private $repo;
+
+    public function __construct(EntityManager $em)
     {
-        $this->repo = $entityManager->getRepository('CurrencyExchangeBundle:CurrencyExchangeRate');
+        $this->em = $em;
+        $this->repo = $em->getRepository('CurrencyExchangeBundle:CurrencyExchangeRate');
     }
 
-    //checks to see if it's been more than 3 hours since last currency exchange rate update
-    public function checkCache()
+    public function queryCurrencyRateData($url)
     {
-        
+        $ch = curl_init();
+        $timeout = 0;
+        curl_setopt ($ch, CURLOPT_URL, $url);
+        curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        curl_setopt ($ch, CURLOPT_USERAGENT,
+            "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
+        curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $rawdata = curl_exec($ch);
+        curl_close($ch);
+
+        return $rawdata;
     }
 
-    public function persist($provider, $from, $to, $rate):bool 
+    public function getCurrencyRate($provider, $from, $to): CurrencyExchangeRate
     {
-        
-    }
+        $repo = $this->em->getRepository('CurrencyExchangeBundle:CurrencyExchangeRate');
+        $saved_rate = $repo->find($provider);
 
+        if ($saved_rate) {
+            $diff = $saved_rate->getDatetimeUpdated()->diff(new \DateTime());
+            if (($diff->h > 3) || ($diff->d > 0) || ($diff->m > 0) || ($diff->y > 0)) {
+                $currency_exchange_rate = $this->queryCurrencyRate($from, $to);
+
+                $saved_rate->setRate($currency_exchange_rate->getRate());
+                $saved_rate->setDatetimeUpdated($currency_exchange_rate->getDatetimeUpdated());
+
+                $this->em->flush();
+
+                return $saved_rate;
+            } else {
+                return $saved_rate;
+            }
+        } else {
+            $currency_exchange_rate = $this->queryCurrencyRate($from, $to);
+
+            $this->em->persist($currency_exchange_rate);
+            $this->em->flush();
+
+            return $currency_exchange_rate;
+        }
+    }
 }
--- src/SauliusVaitkevicius/Bundle/CurrencyExchangeBundle/Service/Provider/CurrencyRatesYahoo.php
+++ src/SauliusVaitkevicius/Bundle/CurrencyExchangeBundle/Service/Provider/CurrencyRatesYahoo.php
@@ -2,6 +2,7 @@
 
 namespace SauliusVaitkevicius\Bundle\CurrencyExchangeBundle\Service\Provider;
 
+use SauliusVaitkevicius\Bundle\CurrencyExchangeBundle\Service\CurrencyRates;
 use SauliusVaitkevicius\Bundle\CurrencyExchangeBundle\Service\CurrencyRatesInterface;
 use Doctrine\ORM\EntityManager;
 use SauliusVaitkevicius\Bundle\CurrencyExchangeBundle\Entity\CurrencyExchangeRate;
@@ -9,31 +10,19 @@ use SauliusVaitkevicius\Bundle\CurrencyExchangeBundle\Entity\CurrencyExchangeRat
 class CurrencyRatesYahoo implements CurrencyRatesInterface
 {
     private $provider = 'Yahoo';
-    private $em;
-    private $repo;
+    private $currency_rates_service;
     
-    public function __construct(EntityManager $em)
+    public function __construct(CurrencyRates $currency_rates_service)
     {
-        $this->em = $em;
-        $this->repo = $em->getRepository('CurrencyExchangeBundle:CurrencyExchangeRate');
+        $this->currency_rates_service = $currency_rates_service;
     }
 
-    //TODO perkelti visa logika i CurrencyRates servisa ir cia tik naudoti metodus is jo
     public function queryCurrencyRate($from, $to): CurrencyExchangeRate
     {
         $url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("' . $from . $to . '")&env=store://datatables.org/alltableswithkeys';
 
-        $ch = curl_init();
-        $timeout = 0;
-        curl_setopt ($ch, CURLOPT_URL, $url);
-        curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        curl_setopt ($ch, CURLOPT_USERAGENT,
-            "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
-        curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-        $rawdata = curl_exec($ch);
-        curl_close($ch);
-
+        $rawdata = $this->currency_rates_service->queryCurrencyRateData($url);
+        
         $pattern = "/(0\.\d{4})/";
         preg_match($pattern, $rawdata, $matches);
         $rate = $matches[0];
@@ -45,30 +34,6 @@ class CurrencyRatesYahoo implements CurrencyRatesInterface
 
     public function getCurrencyRate($from, $to): CurrencyExchangeRate
     {
-        $repo = $this->em->getRepository('CurrencyExchangeBundle:CurrencyExchangeRate');
-        $saved_rate = $repo->find($this->provider);
-
-        if ($saved_rate) {
-            $diff = $saved_rate->getDatetimeUpdated()->diff(new \DateTime());
-            if (($diff->h > 3) || ($diff->d > 0) || ($diff->m > 0) || ($diff->y > 0)) {
-                $currency_exchange_rate = $this->queryCurrencyRate($from, $to);
-                
-                $saved_rate->setRate($currency_exchange_rate->getRate());
-                $saved_rate->setDatetimeUpdated($currency_exchange_rate->getDatetimeUpdated());
-
-                $this->em->flush();
-                
-                return $saved_rate;
-            } else {
-                return $saved_rate;
-            }
-        } else {
-            $currency_exchange_rate = $this->queryCurrencyRate($from, $to);
-
-            $this->em->persist($currency_exchange_rate);
-            $this->em->flush();
-            
-            return $currency_exchange_rate;
-        }
+        return $this->currency_rates_service->getCurrencyRate($this->provider, $from, $to);
     }
 }
