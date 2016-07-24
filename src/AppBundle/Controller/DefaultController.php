5bcae35c66748fd5d85d925bb4b73164ad41a458
Saulius V <Saulius V>
2016-07-24 18:23:29 +0300

    cleaned up a little

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,17 +15,12 @@ class DefaultController extends Controller
     {
         //All of this is just for your ease of testing CurrencyExchangeBundle
         
-//        echo $this->get('currency_exchange.currency_rates')->getBestCurrencyRate("USD", "EUR");
-        
+//        var_dump($this->get('currency_exchange.rate_best')->getBestCurrencyRate("USD", "EUR"));
 //        var_dump($this->get('currency_exchange.currency_rates')->getCurrencyRates("USD", "EUR"));
-        
-//        var_dump($this->get('currency_exchange.rate_best')->getBestCurrencyRate('EUR', "USD"));
-        
 //        foreach ($this->get('currency_exchange.rates')->getCurrencyRates('EUR', "USD") as $provider) {
 //            var_dump($provider);
 //        }
-        
-        var_dump($this->get('currency_exchange.currency_rates_google')->getCurrencyRate("EUR", 'USD'));
+//        var_dump($this->get('currency_exchange.currency_rates_google')->getCurrencyRate("EUR", 'USD'));
         
         return $this->render('base.html.twig');
     }
--- src/SauliusVaitkevicius/Bundle/CurrencyExchangeBundle/Command/CurrencyRateBestCommand.php
+++ src/SauliusVaitkevicius/Bundle/CurrencyExchangeBundle/Command/CurrencyRateBestCommand.php
@@ -5,9 +5,7 @@ namespace SauliusVaitkevicius\Bundle\CurrencyExchangeBundle\Command;
 use SauliusVaitkevicius\Bundle\CurrencyExchangeBundle\Entity\CurrencyExchangeRate;
 use SauliusVaitkevicius\Bundle\CurrencyExchangeBundle\Service\ExchangeRateProvidersCollection;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CurrencyRateBestCommand extends ContainerAwareCommand
